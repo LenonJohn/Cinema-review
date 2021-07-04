a6d07e26ac23f0d4eb779f2b45ed7f9b3d81837f
LenonJohn <main.pc.1996.12.15@gmail.com>
2021-07-04 06:21:19 +0000

    [add]Natural Language API

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,10 +2,16 @@ class PostCommentsController < ApplicationController
   
   def create
     review = Review.find(params[:review_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.review_id = review.id
-    comment.save
-    redirect_to review_path(review)
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.review_id = review.id
+    if @post_comment.save
+      redirect_to review_path(review)
+    else
+      @review = Review.find(params[:review_id])
+      @post_tags = @review.tags
+      @all_comments = PostComment.all
+      render template: "reviews/show"
+    end
   end
 
   def destroy
